a17323905232fdc91ced78b7c7391af4deb04572
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-04-08 07:38:22 -0400

    Removed fonts#

--- boot/boot.asm
+++ boot/boot.asm
@@ -26,6 +26,14 @@ global _start
 _start:
     cli
 
+    ; Enable sse
+    mov eax, cr0
+    and ax, 0xFFFB		;clear coprocessor emulation CR0.EM
+    or ax, 0x2			;set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
 
     ; Pass the multiboot info to the kernel
     push ebx
