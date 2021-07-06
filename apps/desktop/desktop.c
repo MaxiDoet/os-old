2be976919cc9b67dd8ab80fb25463fa1a96ec245
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-07-06 17:46:02 +0000

    Added new logo

--- apps/desktop/desktop.c
+++ apps/desktop/desktop.c
@@ -103,7 +103,7 @@ void desktop_init(unsigned long fbaddr, int width, int height, int pitch)
 	mouse_add_callback(mouse_handler);
 
 	for (int i=0; i < 1000; i++) {
-		draw_image_transparent(main_context, main_context.width / 2 - 50, main_context.height / 2 - 50, 100, 100, logo);
+		draw_image_transparent(main_context, main_context.width / 2 - 215 / 2, main_context.height / 2 - 160 / 2, 215, 160, logo);
 
 		desktop_swap_fb();
 
