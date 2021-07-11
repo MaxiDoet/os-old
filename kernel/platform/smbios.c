506742e2cfa8ab7f5c0d396ff6feb683ef2f7dd6
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-07-11 14:59:13 +0200

    Removed test code

--- kernel/platform/smbios.c
+++ kernel/platform/smbios.c
@@ -20,11 +20,6 @@ smbios_entry_point *smbios_get_entry_point()
 	return entry_point;
 }
 
-uint8_t *smbios_mem = (uint8_t *) 0xF0000;
-        while ((unsigned int) smbios_mem < 0x100000) {
-                if (smbios_mem[0] == '_' && smbios_mem[1] == 'S' && smbios_mem[2] == 'M' && smbios_mem[3] == '_') {
-                        smbios_entry_point *entry_point = (smbios_entry_point *) smbios_mem;
-
 int smbios_get_int(size_t offset, smbios_structure_header *header, int index)
 {
 	uint8_t *p = (uint8_t *) offset + index;
